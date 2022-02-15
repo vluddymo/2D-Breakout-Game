c14ef6308c834ba82f46e95d31d4d2a13358f5fa
vluddymo <wladi.moser@gmail.com>
2022-02-15 16:16:13 +0100

    Make user block moveable

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
 
-const userStart = [230 ,10]
-let currentPosition = userStart
+const userStart = 230;
+let currentPosition = userStart;
 
 // Create Block
 class Block {
@@ -17,21 +17,21 @@ class Block {
 
 //All my Blocks
 const blocks = [
-    new Block(10, 270),
-    new Block(120, 270),
-    new Block(230, 270),
-    new Block(340, 270),
-    new Block(450, 270),
-    new Block(10, 240),
-    new Block(120, 240),
-    new Block(230, 240),
-    new Block(340, 240),
-    new Block(450, 240),
-    new Block(10, 210),
-    new Block(120, 210),
-    new Block(230, 210),
-    new Block(340, 210),
-    new Block(450, 210),
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210),
 ];
 
 // Draw my Block
@@ -48,8 +48,33 @@ function addBlock() {
 addBlock();
 
 // Add user
-const user = document.createElement('div')
-user.classList.add('user')
-user.style.left = currentPosition[0] + 'px'
-user.style.bottom = currentPosition[1] + 'px'
-grid.appendChild(user)
+const user = document.createElement("div");
+user.classList.add("user");
+drawUser();
+grid.appendChild(user);
+
+// Draw user
+function drawUser() {
+  user.style.left = currentPosition + "px";
+}
+
+// Move user
+
+function moveUser(e) {
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentPosition >= 15) {
+        currentPosition -= 15;
+        drawUser();
+      }
+      break;
+    case "ArrowRight":
+      if (currentPosition <= 445) {
+        currentPosition += 15;
+        drawUser();
+      }
+      break;
+  }
+}
+
+document.addEventListener("keydown", moveUser);
