4261d633d7e02e35da550043881eb54220be4166
vluddymo <wladi.moser@gmail.com>
2022-02-26 16:44:29 +0100

    Distribute code into modules

---
+++ modules/constants/constants.mjs
@@ -0,0 +1,7 @@
+const BLOCK_WIDTH = 100;
+const BLOCK_HEIGHT = 20;
+
+export {
+    BLOCK_WIDTH,
+    BLOCK_HEIGHT
+}
--- modules/utils.mjs
+++
@@ -1,5 +0,0 @@
-export default function emptyContainer(container){
-    while (container.firstChild){
-        container.removeChild(container.lastChild)
-    }
-}
